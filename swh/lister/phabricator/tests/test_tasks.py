df2754e5a68150cff225e072c62d19025d647dec
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-06-18 14:12:55 +0200

    phabricator.tasks: Remove unused code
    Related T1824
    Related P438

--- swh/lister/phabricator/tests/test_tasks.py
+++ swh/lister/phabricator/tests/test_tasks.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 
 def test_ping(swh_app, celery_session_worker):
     res = swh_app.send_task(
@@ -8,23 +6,3 @@ def test_ping(swh_app, celery_session_worker):
     res.wait()
     assert res.successful()
     assert res.result == 'OK'
-
-
-@patch('swh.lister.phabricator.tasks.PhabricatorLister')
-def test_incremental(lister, swh_app, celery_session_worker):
-    # setup the mocked PhabricatorLister
-    lister.return_value = lister
-    lister.db_last_index.return_value = 42
-    lister.run.return_value = None
-
-    res = swh_app.send_task(
-        'swh.lister.phabricator.tasks.IncrementalPhabricatorLister')
-    assert res
-    res.wait()
-    assert res.successful()
-
-    lister.assert_called_once_with(
-        api_token=None, forge_url='https://forge.softwareheritage.org',
-        instance='swh')
-    lister.db_last_index.assert_called_once_with()
-    lister.run.assert_called_once_with(min_bound=42)
