fc92c79b7ec53c7ab662071bb7f964177ed9386b
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2019-06-18 07:06:09 +0200

    models: Unify tablenames using singular as main archive's convention
    Related P434

--- swh/lister/bitbucket/models.py
+++ swh/lister/bitbucket/models.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2018 the Software Heritage developers
+# Copyright (C) 2017-2019 the Software Heritage developers
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
@@ -9,7 +9,7 @@ from swh.lister.core.models import IndexingModelBase
 
 class BitBucketModel(IndexingModelBase):
     """a BitBucket repository"""
-    __tablename__ = 'bitbucket_repos'
+    __tablename__ = 'bitbucket_repo'
 
     uid = Column(String, primary_key=True)
     indexable = Column(String, index=True)
