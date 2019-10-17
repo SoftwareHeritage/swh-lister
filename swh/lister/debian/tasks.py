78105940ff2f5262f18b77d77faa128b75bbf419
Nicolas Dandrimont <nicolas@dandrimont.eu>
2019-10-17 14:42:35 +0200

    Stop binding tasks to a specific instance of the celery app
    The celery.shared_task decorator allows late-binding of tasks to any celery app,
    which is well suited for our "task plugin" architecture.

--- swh/lister/debian/tasks.py
+++ swh/lister/debian/tasks.py
@@ -2,17 +2,17 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from swh.scheduler.celery_backend.config import app
+from celery import shared_task
 
 from .lister import DebianLister
 
 
-@app.task(name=__name__ + '.DebianListerTask')
+@shared_task(name=__name__ + '.DebianListerTask')
 def list_debian_distribution(distribution, **lister_args):
     '''List a Debian distribution'''
     DebianLister(**lister_args).run(distribution)
 
 
-@app.task(name=__name__ + '.ping')
+@shared_task(name=__name__ + '.ping')
 def _ping():
     return 'OK'
