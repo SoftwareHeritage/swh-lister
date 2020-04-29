566294749e3b2a9373879a6862864cf17aa00066
Stefano Zacchiroli <zack@upsilon.cc>
2020-04-29 18:31:45 +0200

    setup.py: add documentation link

--- setup.py
+++ setup.py
@@ -79,5 +79,6 @@ setup(
         "Bug Reports": "https://forge.softwareheritage.org/maniphest",
         "Funding": "https://www.softwareheritage.org/donate",
         "Source": "https://forge.softwareheritage.org/source/swh-lister",
+        "Documentation": "https://docs.softwareheritage.org/devel/swh-lister/",
     },
 )
