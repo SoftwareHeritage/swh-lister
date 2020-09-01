250160ad75d9a8ba13f29441ba8bf4f2e57ed9c9
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2020-09-01 15:53:26 +0200

    launchpad: Add missing copyright headers

--- swh/lister/launchpad/tests/test_lister.py
+++ swh/lister/launchpad/tests/test_lister.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2020  The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+
 def test_launchpad_lister(lister_launchpad, datadir):
 
     lister_launchpad.run()
