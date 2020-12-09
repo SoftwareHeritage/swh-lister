b63aa83b41c5b91c8da6e39d6454db46cd4d6ed4
Nicolas Dandrimont <nicolas@dandrimont.eu>
2020-12-09 18:15:28 +0100

    Reimplement the GitHub lister using the new pattern class
    This replaces the test data with some manually generated answers, which allows
    us to test a few more cases for instantiating the lister.
    
    This also expands test coverage to test behavior on rate-limited requests.

--- swh/lister/github/tasks.py
+++ swh/lister/github/tasks.py
@@ -3,42 +3,46 @@
 # See top-level LICENSE file for more information
 
 import random
+from typing import Dict, Optional
 
 from celery import group, shared_task
 
 from swh.lister.github.lister import GitHubLister
 
-GROUP_SPLIT = 10000
+GROUP_SPLIT = 100000
 
 
 @shared_task(name=__name__ + ".IncrementalGitHubLister")
-def list_github_incremental(**lister_args):
+def list_github_incremental() -> Dict[str, int]:
     "Incremental update of GitHub"
-    lister = GitHubLister(**lister_args)
-    return lister.run(min_bound=lister.db_last_index(), max_bound=None)
+    lister = GitHubLister.from_configfile()
+    return lister.run().dict()
 
 
 @shared_task(name=__name__ + ".RangeGitHubLister")
-def _range_github_lister(start, end, **lister_args):
-    lister = GitHubLister(**lister_args)
-    return lister.run(min_bound=start, max_bound=end)
+def _range_github_lister(first_id: int, last_id: int) -> Dict[str, int]:
+    lister = GitHubLister.from_configfile(first_id=first_id, last_id=last_id)
+    return lister.run().dict()
 
 
 @shared_task(name=__name__ + ".FullGitHubRelister", bind=True)
-def list_github_full(self, split=None, **lister_args):
+def list_github_full(self, split: Optional[int] = None) -> str:
     """Full update of GitHub
 
     It's not to be called for an initial listing.
 
     """
-    lister = GitHubLister(**lister_args)
-    ranges = lister.db_partition_indices(split or GROUP_SPLIT)
-    if not ranges:
-        self.log.info("Nothing to list")
-        return
+    lister = GitHubLister.from_configfile()
+    last_index = lister.state.last_seen_id
+
+    bounds = list(range(0, last_index + 1, split or GROUP_SPLIT))
+    if bounds[-1] != last_index:
+        bounds.append(last_index)
+
+    ranges = list(zip(bounds[:-1], bounds[1:]))
     random.shuffle(ranges)
     promise = group(
-        _range_github_lister.s(minv, maxv, **lister_args) for minv, maxv in ranges
+        _range_github_lister.s(first_id=minv, last_id=maxv) for minv, maxv in ranges
     )()
     self.log.debug("%s OK (spawned %s subtasks)" % (self.name, len(ranges)))
     try:
@@ -50,5 +54,5 @@ def list_github_full(self, split=None, **lister_args):
 
 
 @shared_task(name=__name__ + ".ping")
-def _ping():
+def _ping() -> str:
     return "OK"
