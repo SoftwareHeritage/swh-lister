b48f71ff932b190c83003488824135b6aa5d22e6
Antoine Lambert <antoine.lambert@inria.fr>
2021-01-11 16:22:17 +0100

    phabricator/tasks: Allow to pass api_token as optional parameter
    This is useful when one wants to test the lister in docker environment.

--- swh/lister/phabricator/tasks.py
+++ swh/lister/phabricator/tasks.py
@@ -1,6 +1,8 @@
-# Copyright (C) 2019 the Software Heritage developers
+# Copyright (C) 2019-2021 the Software Heritage developers
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
+
+from typing import Optional
 
 from celery import shared_task
 
@@ -8,9 +10,11 @@ from swh.lister.phabricator.lister import PhabricatorLister
 
 
 @shared_task(name=__name__ + ".FullPhabricatorLister")
-def list_phabricator_full(url: str, instance: str):
+def list_phabricator_full(url: str, instance: str, api_token: Optional[str] = None):
     """Full update of a Phabricator instance"""
-    return PhabricatorLister.from_configfile(url=url, instance=instance).run()
+    return PhabricatorLister.from_configfile(
+        url=url, instance=instance, api_token=api_token
+    ).run()
 
 
 @shared_task(name=__name__ + ".ping")
