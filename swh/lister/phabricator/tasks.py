c7822752967124890ffcec9f5733fd471f0ccdc0
Antoine Lambert <antoine.lambert@inria.fr>
2021-01-11 16:27:03 +0100

    phabricator/tasks: Fix task function return type
    Previously, the following error was raised when the task has finished
    its execution: "Object of type ListerStats is not JSON serializable".
    
    So ensure ListerStats object gets converted to dict before returning it.
    
    Also add missing test for task function.

--- swh/lister/phabricator/tasks.py
+++ swh/lister/phabricator/tasks.py
@@ -2,7 +2,7 @@
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from typing import Optional
+from typing import Dict, Optional
 
 from celery import shared_task
 
@@ -10,11 +10,17 @@ from swh.lister.phabricator.lister import PhabricatorLister
 
 
 @shared_task(name=__name__ + ".FullPhabricatorLister")
-def list_phabricator_full(url: str, instance: str, api_token: Optional[str] = None):
+def list_phabricator_full(
+    url: str, instance: str, api_token: Optional[str] = None
+) -> Dict[str, int]:
     """Full update of a Phabricator instance"""
-    return PhabricatorLister.from_configfile(
-        url=url, instance=instance, api_token=api_token
-    ).run()
+    return (
+        PhabricatorLister.from_configfile(
+            url=url, instance=instance, api_token=api_token
+        )
+        .run()
+        .dict()
+    )
 
 
 @shared_task(name=__name__ + ".ping")
