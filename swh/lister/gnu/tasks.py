4cf0c7f7651b2095b2ab63ebc55fcd9eef3a3dd1
Antoine Lambert <antoine.lambert@inria.fr>
2021-01-29 11:49:59 +0100

    gnu: Reimplement lister using new Lister API
    ISO functionalities port of the stateless GNU lister to the new
    swh.lister.pattern.Lister API.
    
    Closes T2990

--- swh/lister/gnu/tasks.py
+++ swh/lister/gnu/tasks.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 the Software Heritage developers
+# Copyright (C) 2019-2021 the Software Heritage developers
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
@@ -10,7 +10,7 @@ from .lister import GNULister
 @shared_task(name=__name__ + ".GNUListerTask")
 def list_gnu_full(**lister_args):
     """List lister for the GNU source code archive"""
-    return GNULister(**lister_args).run()
+    return GNULister.from_configfile(**lister_args).run().dict()
 
 
 @shared_task(name=__name__ + ".ping")
--- swh/lister/gnu/tests/test_tasks.py
+++ swh/lister/gnu/tests/test_tasks.py
@@ -1,9 +1,9 @@
-# Copyright (C) 2019-2020  The Software Heritage developers
+# Copyright (C) 2019-2021  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
-from unittest.mock import patch
+from swh.lister.pattern import ListerStats
 
 
 def test_ping(swh_scheduler_celery_app, swh_scheduler_celery_worker):
@@ -14,17 +14,17 @@ def test_ping(swh_scheduler_celery_app, swh_scheduler_celery_worker):
     assert res.result == "OK"
 
 
-@patch("swh.lister.gnu.tasks.GNULister")
-def test_lister(lister, swh_scheduler_celery_app, swh_scheduler_celery_worker):
-    # setup the mocked GNULister
-    lister.return_value = lister
-    lister.run.return_value = None
+def test_lister(swh_scheduler_celery_app, swh_scheduler_celery_worker, mocker):
+    lister = mocker.patch("swh.lister.gnu.tasks.GNULister")
+    lister.from_configfile.return_value = lister
+    stats = ListerStats(pages=1, origins=300)
+    lister.run.return_value = stats
 
     res = swh_scheduler_celery_app.send_task("swh.lister.gnu.tasks.GNUListerTask")
     assert res
     res.wait()
     assert res.successful()
+    assert res.result == stats.dict()
 
-    lister.assert_called_once_with()
-    lister.db_last_index.assert_not_called()
+    lister.from_configfile.assert_called_once_with()
     lister.run.assert_called_once_with()
