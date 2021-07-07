df46b220984d7187f0ef193f006d17cfd04cc41a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-07-07 16:14:09 +0200

    Make PyPI lister incremental and complete in regards to last_update
    This rewrote the current implementation to actually use pypi's xml-rpc api which allows
    to be incremental. It also allows to fetch the last release date per package. This last
    part actually make it possible to update the "last_update" entry in the ListedOrigin
    model.
    
    Related to T3399

--- swh/lister/pypi/tasks.py
+++ swh/lister/pypi/tasks.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 the Software Heritage developers
+# Copyright (C) 2018-2021 the Software Heritage developers
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
@@ -7,7 +7,7 @@ from celery import shared_task
 from .lister import PyPILister
 
 
-@shared_task(name=__name__ + ".PyPIListerTask")
+@shared_task(name=f"{__name__}.PyPIListerTask")
 def list_pypi():
     "Full listing of the PyPI registry"
     lister = PyPILister.from_configfile()
