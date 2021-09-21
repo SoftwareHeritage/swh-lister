e7716c0122e9c4a411ed54dde821b48939eb2ebb
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2021-09-21 18:18:49 +0200

    opam: Share opam root directory even on multiple instances
    That avoids having multiple distinct opam root directories per opam lister instance. The
    current opam commands used by the lister are actually listing specifically per instance.
    
    Related to P1171

--- swh/lister/opam/tests/test_lister.py
+++ swh/lister/opam/tests/test_lister.py
@@ -4,12 +4,13 @@
 # See top-level LICENSE file for more information
 
 import io
+import os
 from tempfile import mkdtemp
 from unittest.mock import MagicMock
 
 import pytest
 
-from swh.lister.opam.lister import OpamLister
+from swh.lister.opam.lister import OpamLister, opam_init
 
 module_name = "swh.lister.opam.lister"
 
@@ -29,6 +30,45 @@ def mock_opam(mocker):
     return mock_init, mock_open
 
 
+def test_mock_init_repository_init(mock_opam, tmp_path, datadir):
+    """Initializing opam root directory with an instance should be ok
+
+    """
+    mock_init, mock_popen = mock_opam
+
+    instance = "fake"
+    instance_url = f"file://{datadir}/{instance}"
+    opam_root = str(tmp_path / "test-opam")
+    assert not os.path.exists(opam_root)
+
+    # This will initialize an opam directory with the instance
+    opam_init(opam_root, instance, instance_url, {})
+
+    assert mock_init.called
+
+
+def test_mock_init_repository_update(mock_opam, tmp_path, datadir):
+    """Updating opam root directory with another instance should be ok
+
+    """
+    mock_init, mock_popen = mock_opam
+
+    instance = "fake_opam_repo"
+    instance_url = f"file://{datadir}/{instance}"
+    opam_root = str(tmp_path / "test-opam")
+
+    os.makedirs(opam_root, exist_ok=True)
+    with open(os.path.join(opam_root, "opam"), "w") as f:
+        f.write("one file to avoid empty folder")
+
+    assert os.path.exists(opam_root)
+    assert os.listdir(opam_root) == ["opam"]  # not empty
+    # This will update the repository opam with another instance
+    opam_init(opam_root, instance, instance_url, {})
+
+    assert mock_init.called
+
+
 def test_lister_opam_optional_instance(swh_scheduler):
     """Instance name should be optional and default to be built out of the netloc."""
     netloc = "opam.ocaml.org"
@@ -36,21 +76,19 @@ def test_lister_opam_optional_instance(swh_scheduler):
 
     lister = OpamLister(swh_scheduler, url=instance_url,)
     assert lister.instance == netloc
-    assert lister.opamroot.endswith(lister.instance)
+    assert lister.opam_root == "/tmp/opam/"
 
 
 def test_urls(swh_scheduler, mock_opam):
     mock_init, mock_popen = mock_opam
-
     instance_url = "https://opam.ocaml.org"
+    tmp_folder = mkdtemp(prefix="swh_opam_lister")
 
     lister = OpamLister(
-        swh_scheduler,
-        url=instance_url,
-        instance="opam",
-        opam_root=mkdtemp(prefix="swh_opam_lister"),
+        swh_scheduler, url=instance_url, instance="opam", opam_root=tmp_folder,
     )
     assert lister.instance == "opam"
+    assert lister.opam_root == tmp_folder
 
     # call the lister and get all listed origins urls
     stats = lister.run()
@@ -75,6 +113,35 @@ def test_urls(swh_scheduler, mock_opam):
 
 
 def test_opam_binary(datadir, swh_scheduler):
+    instance_url = f"file://{datadir}/fake_opam_repo"
+
+    lister = OpamLister(
+        swh_scheduler,
+        url=instance_url,
+        instance="fake",
+        opam_root=mkdtemp(prefix="swh_opam_lister"),
+    )
+
+    stats = lister.run()
+
+    assert stats.pages == 4
+    assert stats.origins == 4
+
+    scheduler_origins = swh_scheduler.get_listed_origins(lister.lister_obj.id).results
+
+    expected_urls = [
+        f"opam+{instance_url}/packages/agrid/",
+        f"opam+{instance_url}/packages/calculon/",
+        f"opam+{instance_url}/packages/directories/",
+        f"opam+{instance_url}/packages/ocb/",
+    ]
+
+    result_urls = [origin.url for origin in scheduler_origins]
+
+    assert expected_urls == result_urls
+
+
+def test_opam_multi_instance(datadir, swh_scheduler):
     instance_url = f"file://{datadir}/fake_opam_repo"
 
     lister = OpamLister(
