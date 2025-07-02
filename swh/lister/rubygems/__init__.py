0612839a8ff71194a667e970cbc3698927c15bdc
Antoine Lambert <anlambert@softwareheritage.org>
2025-07-02 14:03:42 +0200

    rubygems: Update documentation describing how the lister is working
    Fixes #4718.

--- swh/lister/rubygems/__init__.py
+++ swh/lister/rubygems/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022  The Software Heritage developers
+# Copyright (C) 2022-2025  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
@@ -8,27 +8,41 @@
 RubyGems lister
 ===============
 
-The RubyGems lister list origins from `RubyGems.org`_, the Ruby community’s gem hosting service.
+The RubyGems lister list origins from `RubyGems.org`_, the Ruby community's gem hosting service.
 
-As of September 2022 `RubyGems.org`_ list 173384 package names.
+As of July 2025 `RubyGems.org`_ list 186,003 package names.
 
 Origins retrieving strategy
 ---------------------------
 
-To get a list of all package names we call an `http endpoint`_ which returns a list of gems
-as text.
+To list all available gems and retrieve relevant data about gems in a performant way, the daily
+`PostgreSQL database dump`_ of RubyGems, is exploited.
+
+All gems are listed by executing the following query:
+
+.. code-block:: sql
+
+   SELECT id, name FROM rubygems
+
+Relevant listing data are then retrieved by executing that query for each gem:
+
+.. code-block:: sql
+
+   SELECT built_at, full_name, number, sha256, size
+   FROM versions
+   WHERE rubygem_id = <gem_id> AND yanked_at IS NULL
 
 Page listing
 ------------
 
-Each page returns an origin url based on the following pattern::
+Each page returns listing info about one gem, its origin url is based on the following pattern::
 
-    https://rubygems.org/gems/{pkgname}
+    https://rubygems.org/gems/{gem_name}
 
 Origins from page
 -----------------
 
-The lister yields one origin url per page.
+The lister yields one listed origin per page.
 
 Running tests
 -------------
@@ -53,7 +67,7 @@ You can follow lister execution by displaying logs of swh-lister service::
    docker compose logs -f swh-lister
 
 .. _RubyGems.org: https://rubygems.org/
-.. _http endpoint: https://rubygems.org/versions
+.. _PostgreSQL database dump: https://rubygems.org/pages/data
 """
 
 
