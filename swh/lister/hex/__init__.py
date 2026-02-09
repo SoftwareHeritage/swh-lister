5f2241b1f47657f10561c307cb297ac6ac1d3e0a
Antoine R. Dumont (@ardumont) <ardumont@softwareheritage.org>
2026-02-09 11:51:44 +0100

    hex: Adapt default interval to a minimum
    That lister is fast, the default of 90 days is too large.

--- swh/lister/hex/__init__.py
+++ swh/lister/hex/__init__.py
@@ -1,13 +1,22 @@
-# Copyright (C) 2022  The Software Heritage developers
+# Copyright (C) 2022-2026  The Software Heritage developers
 # See the AUTHORS file at the top-level directory of this distribution
 # License: GNU General Public License version 3, or any later version
 # See top-level LICENSE file for more information
 
 
 def register():
+    from datetime import timedelta
+
     from .lister import HexLister
 
     return {
         "lister": HexLister,
         "task_modules": [f"{__name__}.tasks"],
+        "task_types": {
+            "list-hex-full": {
+                "default_interval": timedelta(days=1),
+                "min_interval": timedelta(days=1),
+                "max_interval": timedelta(days=1),
+            },
+        },
     }
